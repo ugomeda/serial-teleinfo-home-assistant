9575c2549a8446ddf876dfe6991ed6d2b48bfc1e
tom <thomas.boudard@univ-nantes.fr>
2025-01-05 01:28:33 +0100

    update code to be compatible with Home Assistant 2025.1.0. In this release, some classes has been deprecated and must be replaced. Also, async_forward_entry_setup must be replaced by async_forward_entry_setups to be compatible with 2025.6 (see https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/)

--- custom_components/teleinformation/__init__.py
+++ custom_components/teleinformation/__init__.py
@@ -108,7 +108,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     }
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
 
     return True
